f888cb3078a2cf3065b5c0ccc76e3a1b06a096ab
hayathine <gwsgsgdas@gmail.com>
2026-02-23 17:48:22 +0900

    add eslint.configs

---
+++ frontend/eslint.config.js
@@ -0,0 +1,40 @@
+import js from "@eslint/js";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+	{
+		ignores: ["dist", "node_modules", "vite.config.js", "tailwind.config.js"],
+	},
+	{
+		extends: [js.configs.recommended, ...tseslint.configs.recommended],
+		files: ["**/*.{ts,tsx}"],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: {
+				...globals.browser,
+				...globals.es2020,
+			},
+		},
+		plugins: {
+			"react-hooks": reactHooks,
+			"react-refresh": reactRefresh,
+		},
+		rules: {
+			...reactHooks.configs.recommended.rules,
+			"react-refresh/only-export-components": "off",
+			"@typescript-eslint/no-explicit-any": "off",
+			"react-hooks/exhaustive-deps": "off",
+			"@typescript-eslint/no-unused-vars": [
+				"warn",
+				{
+					argsIgnorePattern: "^_",
+					varsIgnorePattern: "^_",
+					caughtErrorsIgnorePattern: "^_",
+				},
+			],
+		},
+	},
+);
